347e0efca3d5916c5431217ee94d388c5c21fc9e
Fadil Al <fadil.alfarhan@gmail.com>
2021-06-21 09:38:52 +0200

    Implementing jsonWebToken when register a user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
-const md5 = require('md5');
-const bcrypt = require('bcryptjs');
+const md5 = require('md5')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
@@ -47,10 +49,25 @@ router.post(
       
       await user.save()
 
-      res.send('User registered')
+      const payload = { 
+        user: { 
+          id: user.id,
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if(err) throw err
+          res.json({ token })
+        }
+      )
+
     } catch (err) {
       console.error(err.message)
-      return res.status(500).send('Sever Error')
+      res.status(500).send('Sever Error')
     }
 
   }
