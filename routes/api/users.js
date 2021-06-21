394b259fa3adbf061cffaeef1cafa0e6931267d5
Fadil Al <fadil.alfarhan@gmail.com>
2021-06-21 09:08:28 +0200

    User registration fixed, hashing the avatar-email to md5 before storing to db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
+const md5 = require('md5');
+const bcrypt = require('bcryptjs');
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
+
+const User = require('../../models/User')
 
 // @route    POST api/users
 // @desc     Register User
@@ -15,13 +19,40 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
 
-    res.send('User Route')
+    const { name, email, password } = req.body
+    
+    try {
+      let user = await User.findOne({ email })
+
+      if(user) {
+        return res.status(400).json({ errors: [{ msg: 'User already exists' }]})
+      }
+
+      user = new User({
+        name,
+        email,
+        avatar: md5(email),
+        password
+      })
+
+      const salt = await bcrypt.genSalt(10)
+
+      user.password = await bcrypt.hash(password, salt)
+      
+      await user.save()
+
+      res.send('User registered')
+    } catch (err) {
+      console.error(err.message)
+      return res.status(500).send('Sever Error')
+    }
+
   }
 )
 
